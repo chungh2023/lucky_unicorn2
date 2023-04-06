848679c3146432549e2b7d7b0964f78768e1b398
chungh2023 <126839629+chungh2023@users.noreply.github.com>
2023-04-06 19:50:48 +0900

    Create 00_LU_base_v1.py

---
+++ 00_LU_base_v1.py
@@ -0,0 +1,67 @@
+"""LU base component
+Components added after they have been created and tested"""
+
+
+# yes/no checking function
+def yes_no(question_text):
+    while True:
+
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
+
+        # If they say yes, output 'Program Continues'
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If they say no, output 'Display Instructions'
+        elif answer == "no" or answer == "n":
+            answer = "No"
+            return answer
+
+        # Otherwise - show error
+        else:
+            print("Please answer 'yes' or 'no'")
+
+
+# function to display instructions
+def instructions():
+    print("**** How to Play *****")
+    print()
+    print("The rules of the game will go here")
+    print()
+
+
+# number checking function
+def num_check(question, low, high):
+    error = "That was not valid input\n" \
+            "Please enter a number between {} and {}\n".format(low, high)
+
+    # Keep asking until a valid amount (1-10) is entered
+    while True:
+        try:
+            # ask for amount
+            response = int(input(question))
+
+            #check for number within the required range
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main routine go here...
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "No":
+    instructions()
+
+
+
+# ask the user how much they want to play with
+user_balance = num_check("How much would you like to play with? $", 1, 10)
+print(f"You are playing with ${user_balance}")
+
