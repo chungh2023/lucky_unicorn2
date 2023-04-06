02d0e36cd23fc58ace52f9693e1a1a84a252c473
chungh2023 <126839629+chungh2023@users.noreply.github.com>
2023-04-06 19:58:46 +0900

    Text formatting applied

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello world")
+text = "hello world"
+sides = "$" * 3
+
+
+formatted_text = f"{sides} {text} {sides}"
+top_bottom = "*" * len(formatted_text)
+
+print(top_bottom)
+print(formatted_text)
+print(top_bottom)
